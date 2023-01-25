94c29696f01eeecc6174b7991fba578f0beab574
Beimy <arbm159357@gmail.com>
2023-01-25 00:55:49 -0500

    started on paintingsingle page

---
+++ src/components/PaintingSingle/index.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useEffect } from 'react';
+import { useSiteContext } from '../../utils/GlobalState';
+
+const PaintingSingle = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PaintingSingle;
--- src/pages/Paintings.jsx
+++ src/pages/Paintings.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSiteContext } from '../utils/GlobalState';
+import PaintingSingle from '../components/PaintingSingle';
+
 
 const PaintingsPage = () => {
 
     return (
         <div className='flex flex-col justify-center'>
             Paintings
-            <img src='https://drive.google.com/drive/u/0/folders/1sh9mOXHcENmJDTqKdbeOVpzlA0bZ1bHx'></img>
+            <PaintingSingle></PaintingSingle>
         </div>
         
     )
